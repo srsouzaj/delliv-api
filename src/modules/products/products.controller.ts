ec341f81170afaa4d8ec49c08a4ce15a530599f6
Jorge de Souza <jorgejrdj1@gmail.com>
2023-07-22 15:20:02 -0300

    feat: finish crud nestJS

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductDTO } from './products.dto';
 
@@ -9,5 +17,20 @@ export class ProductsController {
   @Post()
   async create(@Body() data: ProductDTO) {
     return this.productsService.create(data);
+  }
+
+  @Get()
+  async findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() data: ProductDTO) {
+    return this.productsService.update(id, data);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.productsService.delete(id);
   }
 }
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { ProductDTO } from './products.dto';
 import { PrismaService } from 'src/database/Prisma.service';
@@ -11,5 +12,46 @@ export class ProductsService {
             data,
         });
         return product;
+    }
+
+    async findAll() {
+        return this.prisma.product.findMany();
+    }
+
+    async update(id: string, data: ProductDTO) {
+        const productExists = await this.prisma.product.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!productExists) {
+            throw new Error('Product does not exist');
+        }
+
+        await this.prisma.product.update({
+            data,
+            where: {
+                id,
+            },
+        });
+    }
+
+    async delete(id: string) {
+        const productExists = await this.prisma.product.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if (!productExists) {
+            throw new Error('Product does not exist');
+        }
+
+        await this.prisma.product.delete({
+            where: {
+                id,
+            },
+        });
     }
 }
